0af72821523858af654c278506c14b58678e5c2c
Lucas Bomfim <lucasbomfimsobral@gmail.com>
2024-04-02 10:19:09 -0300

    Adicionando o icon - 02/04/24

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import icon from './assets/images/icon.png' // Importe o ícone aqui
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
@@ -10,6 +10,12 @@ root.render(
     <App />
   </React.StrictMode>
 )
+
+// Adicione o ícone ao head
+const link = document.createElement('link')
+link.rel = 'icon'
+link.href = icon
+document.head.appendChild(link)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
